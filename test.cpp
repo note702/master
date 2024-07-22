aaac5d5891e626f55ee04d28cd421d6eca70d6ad
note702 <sutscr6615.ki@gmail.com>
2024-07-22 22:56:58 +0900

    じぇじぇじぇ

--- test.cpp
+++ test.cpp
@@ -4,15 +4,14 @@
 int main(){
     printf("Hello World");
     printf("good morning");
-<<<<<<<<< Temporary merge branch 1
+
     printf("very good");
 
-=========
+
     printf("ok");
 
 
     printf("information");
     printf("info");
->>>>>>>>> Temporary merge branch 2
-    
+
 }
