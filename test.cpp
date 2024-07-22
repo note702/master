bad3ac967212c992498a709f9c09e225c0e393fe
note702 <sutscr6615.ki@gmail.com>
2024-07-22 22:54:19 +0900

    .\test.cpp

--- test.cpp
+++ test.cpp
@@ -4,14 +4,10 @@
 int main(){
     printf("Hello World");
     printf("good morning");
-<<<<<<< HEAD
+
+
     printf("ok");
 
-    printf("information");
-    printf("info");
-=======
-    printf("very good");
 
->>>>>>> origin
-    
-}
+    printf("information");
+    printf("info");}
